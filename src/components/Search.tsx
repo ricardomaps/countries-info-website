cbe2ce286e223094dd9e1d4eca211d224cbcfcdf
Ricardo Mapurunga Junior <ricmaps_@LAPTOP-LO9AB2OE.localdomain>
2025-06-05 17:08:51 -0300

    improved Search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -22,7 +22,7 @@ export default function Search() {
     <div className="bg-skin-card text-skin-muted py-4 px-8 w-[32rem] rounded-md shadow-md">
       <FontAwesomeIcon icon={faSearch} className="mr-6"/>
       <input 
-        className="inline outline-none"
+        className="inline outline-none bg-transparent"
         onChange={(e) => handleSearch(e.target.value)} 
         defaultValue={params.get("name")?.toString()} 
         type="text" 
